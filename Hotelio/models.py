77195af6e1d0f3b4d234ff0969ee797c2daea28f
Monika <monika.nowicka5@teb.edu.pl>
2024-12-12 21:19:23 +0100

    12.12 - stworzenie modelu 'Hotel'

--- Hotelio/models.py
+++ Hotelio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    rooms_available = models.IntegerField()
+    image = models.ImageField(upload_to='hotel_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
