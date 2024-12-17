86c69e9a38cebe99c414fec41a3606cd7e534960
Monika <monika.nowicka5@teb.edu.pl>
2024-12-17 21:09:12 +0100

    17.12 - blad Account

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'Profil {self.user.username}'
+
