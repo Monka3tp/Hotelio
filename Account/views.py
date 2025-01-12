6ea9761b84c78663013c4402612e9bb3087a31a1
Krystianek3Tp <krystian.hordynski@teb.edu.pl>
2025-01-12 09:50:55 +0100

    dodanie walidacji rejestracji, dodanie widoku mój profil

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
@@ -17,3 +19,9 @@ def signup(request):
 
 def logout_success(request):
     return render(request, 'logout_success.html')
+
+def check_username(request, username):
+    if User.objects.filter(username=username).exists():
+        return HttpResponse("exists")
+    return HttpResponse("available")
+
