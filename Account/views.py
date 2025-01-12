5ad2c3df21acbe814a78f41fba6e629e75ef7ebc
Krystianek3Tp <krystian.hordynski@teb.edu.pl>
2025-01-12 14:58:17 +0100

    edycja pola mój profil, wyswietlanie danych uzytkownika

--- Account/views.py
+++ Account/views.py
@@ -5,16 +5,17 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from Hotelio.forms import CustomUserCreationForm
 
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('hotel_list')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
 
 def logout_success(request):
